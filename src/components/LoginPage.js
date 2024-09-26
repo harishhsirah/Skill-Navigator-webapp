6548ef543ca89e3d270d93f99bbc0067c9779142
harishhsirah <harishbharathan2002@gmail.com>
2024-09-26 15:21:03 +0530

    new staged

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -36,27 +36,90 @@ const LoginPage = ({setIsLoggedIn}) => {
   };
 
   return (
-    <div>
-      <h1>Login Page</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
+    <div className="flex min-h-screen items-center justify-arounf bg-gray-50">
+  <div className="sm:mx-auto sm:w-full sm:max-w-sm animate-fadeIn">
+    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+      Sign in to your account
+    </h2>
+
+    <div className="mt-10">
+      <form onSubmit={handleLogin} className="space-y-6">
+        <div>
+          <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Email"
+              required
+              autoComplete="email"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+            Password
+          </label>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Password"
+              required
+              autoComplete="current-password"
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-around rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
       </form>
-      <p>New here? <button onClick={handleSignupRedirect}>Sign up</button></p>
+
+      <p className="mt-10 text-center text-sm text-gray-500">
+        New here?{' '}
+        <button onClick={handleSignupRedirect} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+          Sign up
+        </button>
+      </p>
     </div>
-  );
-};
+  </div>
+
+  <style jsx>{`
+    @keyframes fadeIn {
+      from {
+        opacity: 0;
+        transform: translateY(20px);
+      }
+      to {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
+
+    .animate-fadeIn {
+      animation: fadeIn 0.5s ease-in-out;
+    }
+  `}</style>
+</div>
+  )
+}
 
 
 export default LoginPage;
